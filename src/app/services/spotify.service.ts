d5522e6ee7997666aff3c327b2ffa64d35b02bd3
MegalLink <50426219+MegalLink@users.noreply.github.com>
2020-04-09 15:07:16 -0500

    Refactoring del service

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,32 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
-import {map} from 'rxjs/operators'
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { map } from "rxjs/operators";
 @Injectable({
-  providedIn: 'root' // aqui le dice que esta proveido, ya no hay que ponerlo en app.module.ts como provider por el decorador
+  providedIn: "root", // aqui le dice que esta proveido, ya no hay que ponerlo en app.module.ts como provider por el decorador
 })
 export class SpotifyService {
+  constructor(private _http: HttpClient) {
+    console.log("Spotify Service listo");
+  }
+  private tokenSpo: string =
+    "BQCo6Rw2cA5AD5CSOX49Ja54tk-CA8iPucXDOqvEtuf6pckYVlM__cHHib1mEjwDIKJGAReldYlanadQax8";
+  getQuery(query:string){
+    const url=`https://api.spotify.com/v1/${query}`;
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.tokenSpo}`,
+    });
+    return this._http.get(url,{headers});
+  }
 
-  constructor(private _http:HttpClient) { 
-    console.log("Spotify Service listo")
+  getNewReleases() {
+   return this.getQuery('browse/new-releases?limit=20').pipe(map((data) => data["albums"].items));
   }
-  private tokenSpo:string ="BQCo6Rw2cA5AD5CSOX49Ja54tk-CA8iPucXDOqvEtuf6pckYVlM__cHHib1mEjwDIKJGAReldYlanadQax8";
-  getNewReleases(){
-    const headers=new HttpHeaders({
-     'Authorization': `Bearer ${this.tokenSpo}`
-    })
-    //https://api.spotify.com/v1/browse/new-releases?limit=5
-     return this._http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{headers}).pipe(map(data=>{
-        return data['albums'].items;
-     }))
-  }
-  getArtista(termino:string){
-    const headers=new HttpHeaders({
-      'Authorization': `Bearer ${this.tokenSpo}`
-     })
-     //https://api.spotify.com/v1/browse/new-releases?limit=5
-      return this._http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,{headers}).pipe(map(data=>{
-      
-        return data['artists'].items;
-     }))
+  getArtista(termino: string) {
+  
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(map((data) => data["artists"].items));
   }
 }
